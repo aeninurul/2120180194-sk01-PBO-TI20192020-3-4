c23d60946b8ee0127a9407b19df4006074724997
aeninurul <aeninurul9999@gmail.com>
2019-09-26 16:03:30 +0700

    upload tugas studi kasus 1

---
+++ 2120180194-SK01-PBOTI20192020-3-4-01/src/pkg2120180194/sk01/pboti20192020/pkg3/pkg4/pkg01/SK01PBOTI201920203401.java
@@ -0,0 +1,18 @@
+
+package pkg2120180194.sk01.pboti20192020.pkg3.pkg4.pkg01;
+public class SK01PBOTI201920203401 { 
+
+    public static void main(String[] args) {
+      int x=10;
+      for (int i=1;i<=x;i++){
+          for(int k=18;k<=i;k --){
+           System.out.print("1");
+          }
+          for(int l=1;l<=i-1;l++){
+            System.out.print("l");
+          }
+          System.out.println();
+       }
+    }
+    
+}
---
+++ 2120180194-SK01-PBOTI20192020-3-4-02/src/pkg2120180194/sk01/pboti20192020/pkg3/pkg4/pkg02/SK01PBOTI201920203402.java
@@ -0,0 +1,27 @@
+package pkg2120180194.sk01.pboti20192020.pkg3.pkg4.pkg02;
+public class SK01PBOTI201920203402 {
+
+    public static void main(String[] args) {
+        String n="              2";
+        String u="             22";
+        String r="            222";
+        String a="           2222";
+        String e="          22222";
+        String y="         222222";
+        String l="        2222222";
+        String j="       22222222";
+        String m="      222222222";
+        
+        System.out.println(n);
+        System.out.println(u);
+        System.out.println(r);
+        System.out.println(a);
+        System.out.println(e);
+        System.out.println(y);
+        System.out.println(l);
+        System.out.println(j);
+        System.out.println(m);
+       
+    }
+    
+}
